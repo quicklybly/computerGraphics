10596ca73e063f366a4f1fbc2d51281b098584fb
quicklybly <quickly.tawer@gmail.com>
2022-10-07 01:14:47 +0300

    added bubble animation

--- src/main/java/ru/vsu/cs/lysenko_a_r/task1/DrawPanel.java
+++ src/main/java/ru/vsu/cs/lysenko_a_r/task1/DrawPanel.java
@@ -1,23 +1,60 @@
 package ru.vsu.cs.lysenko_a_r.task1;
 
 import ru.vsu.cs.lysenko_a_r.task1.elements.Background;
+import ru.vsu.cs.lysenko_a_r.task1.elements.Bubble;
 import ru.vsu.cs.lysenko_a_r.task1.elements.House;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class DrawPanel extends JPanel {
+
+    Background bg;
+    House hs;
+
+    ArrayList<Bubble> bubbles;
+
+    private int t = 0;
+
     public DrawPanel() {
+        bg = new Background();
+        hs = new House();
+        bubbles = new ArrayList<>();
+        bubbles.add(new Bubble(605, 200, 30, 30, new Color(236, 7, 91, 100)));
+        bubbles.add(new Bubble(605, 130, 30, 30, new Color(68, 155, 222, 100)));
+        bubbles.add(new Bubble(605, 50, 30, 30, new Color(53, 216, 113, 100)));
+
+        int delay = 500;
+        Timer timer = new Timer(delay, listener -> {
+            t = (t + delay) % Integer.MAX_VALUE;
+            if (bubbles.stream().allMatch(elem -> elem.getUpperBound() <= 0)) {
+                bubbles.forEach((bubble -> {
+                    bubble.reset();
+                    System.out.println("g");
+                    repaint();
+                }));
+            }
+            bubbles.forEach((bubble -> {
+                bubble.move();
+                repaint();
+            }));
+        });
+        timer.start();
+
+
+
     }
 
     @Override
     public void paint(Graphics gr) {
         super.paint(gr);
         Graphics2D g = (Graphics2D) gr;
-        Background bg = new Background();
         bg.draw(g);
-        House hs = new House();
         hs.draw(g);
+
+        bubbles.forEach((bubble) -> bubble.draw(g));
+
 
     }
 }
---
+++ src/main/java/ru/vsu/cs/lysenko_a_r/task1/elements/Bubble.java
@@ -0,0 +1,48 @@
+package ru.vsu.cs.lysenko_a_r.task1.elements;
+
+
+import java.awt.*;
+import java.util.Random;
+
+public class Bubble {
+    int x, y, shiftY, returnY, size;
+    Color color;
+
+    Random rnd = new Random();
+
+    public Bubble(int x, int y, int shiftY, int size, Color color) {
+        this.x = x;
+        this.y = y;
+        this.shiftY = shiftY;
+        this.size = size;
+        this.color = color;
+        returnY = this.y;
+    }
+
+    public int getUpperBound() {
+        return y;
+    }
+
+    public void move() {
+        this.shiftY += Math.abs(rnd.nextInt() % 2);
+    }
+
+    public void reset() {
+        this.y = returnY;
+    }
+
+    public void draw(Graphics2D g) {
+        Color oldColor = g.getColor();
+        Stroke oldStroke = g.getStroke();
+        y -= shiftY;
+        g.drawOval(x, y, size, size);
+        g.setColor(color);
+        g.fillOval(x, y, size, size);
+        g.setColor(new Color(255, 255, 255, 50));
+        g.fillOval(x, y + size / 3, size / 3, size / 3);
+        g.setColor(oldColor);
+        g.setStroke(oldStroke);
+    }
+
+
+}
